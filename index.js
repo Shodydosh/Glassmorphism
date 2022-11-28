9b0ba66e33bea9f17afd512e491ca65d12d47be7
Tung Nguyen Minh <nguyenminhtung252@gmail.com>
2022-11-28 22:37:35 +0700

    Adding banner slideshow

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let slideIndex = 1;
+
 // Get the button:
 let mybutton = document.getElementById("btt-btn");
 
@@ -19,4 +21,61 @@ function scrollFunction() {
 
 function topFunction() {
   window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+$(document).ready(function () {
+  if (!$.browser.webkit) {
+    $(".wrapper").html("<p>Sorry! Non webkit users. :(</p>");
+  }
+});
+
+//! SLIDE
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  console.log(slides);
+  console.log(slideIndex - 1);
+  console.log(slides[slideIndex - 1]);
+  slides[slideIndex - 1].style.display = " block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function autoSlidin() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(autoSlidin, 2000); // Change image every 2 seconds
 }
